0b938e47971c60c36e2662a16392f9705c618df0
Zukslay <uksturmanboy@gmail.com>
2025-10-22 15:27:12 -0400

    second chapter

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from stats import get_num_words
 from stats import get_total_characters
+from stats import sorted_characters
+import sys 
 
-path = "/home/fabrizio/workflow/github.com/bootdotdev/bookbot/books/frankenstein.txt"
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+path = sys.argv[1]
+temp_dict = sorted_characters(path)
 
 def main():
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     print("Found",get_num_words(path),"total words")
-    print(get_total_characters(path))
+    print("--------- Character Count -------")
+    for a,b in temp_dict.items():
+        print(f"{a}: {b}")
+    print("============= END ===============")
 
 main()
 
--- stats.py
+++ stats.py
@@ -18,3 +18,19 @@ def get_total_characters(file_path):
             else:
                 temp_dict[character.lower()] += 1
         return temp_dict
+    
+def sorted_characters(file_path):
+    with open(file_path) as f:
+        file_contents = f.read()
+        temp_dict = {}
+        
+        for character in file_contents:
+            if not character.isalpha():
+                continue
+            if character.lower() not in temp_dict:
+                temp_dict[character.lower()] = 1
+            else:
+                temp_dict[character.lower()] += 1
+        
+        return dict(sorted(temp_dict.items(), key = lambda x:x[1], reverse = True))
+        
